9dbce6db22f6bc703b41deddf0d8f5ce2a02413c
Patrick Coffey <patrickcoffey48@gmail.com>
2014-06-29 23:08:11 -0500

    Add db and collection, and pushEntity beginnings

--- classes/MongoDBEntity.php
+++ classes/MongoDBEntity.php
@@ -4,6 +4,7 @@
  *
  * Defines a class for pushing/pulling entites to/from a MongoDB
  * @param (string) $host path to the MongoDB instance we should use
+ * @param (string) $db database we should push entities too
  * @param (string) $user username for the MongoDB instance we should use
  * @param (string) $pwd password for the MongoDB instance we should use
  *
@@ -11,12 +12,29 @@
  */
 
 class MongoDBEntity {
-  private $mongo; // MongoClient
+  private $mongo;  // MongoClient
+  private $db;     // Database as specified in the __construct
 
   /**
    * Initializes a MongoDBEntity object.
    */
-  function __construct($host, $user,$pwd) {
-    $this->mongo = new MongoClient("mongodb://{$host}", array('username' => $user, 'password' => $pwd));
+  function __construct($host, $db, $user, $pwd) {
+    $options = array(
+      'username' => $user,
+      'password' => $pwd
+    );
+
+    $this->mongo = new MongoClient("mongodb://{$host}", $options);
+    $this->db = $this->mongo->{$db};
+  }
+
+  /**
+   * Pushes an entity to a MongoDB Collection
+   * @param (object) $entity entity that we will send to the mongo collection
+   * @param (string) $collection collection that we will send the entity to
+   */
+  public function pushEntity($entity, $collection) {
+    $collection = $this->db->{$collection};
+    $collection->insert($entity);
   }
 }
